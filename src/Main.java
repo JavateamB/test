8058ad010f921704e4a1b8a35c6dd377b6f99019
JavateamB <lucasr3@umbc.edu>
2025-02-24 21:43:16 -0500

    cojbladbfyvou

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,6 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("hello");
         System.out.println("This is Lucas");
+        System.out.println("please work");
+    }
 }
